90577f55cf5a103c5664f8521928e325b14f02d7
Joe Watson SBF <joewatson40@hotmail.com>
2021-09-07 00:37:36 -0500

    Se hace los filtros de los productos

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const dao = require('./dao/fakeData');
+const { getElectrodomesticosGeneral, getNeveras , getTelevisores } = require('./service/Factura');
+const lista = dao.generate();
 
-console.log(dao.generate())
+console.log(getNeveras(lista))
+console.log("\n\n")
+console.log(getElectrodomesticosGeneral(lista))
+console.log("\n\n")
+console.log(getTelevisores(lista))
+/*
+const listTvs = lista.filter(data => data.tdt !== undefined);
+listTvs.forEach(element => {
+  console.log(element.obtenerPrecio())
+});
+console.log("Vs: ", listTvs[0].obtenerPrecio())
+*/
--- service/Factura.js
+++ service/Factura.js
@@ -3,6 +3,16 @@ const Televisor = require('../model/Televisor')
 const ElectrodomesticoGeneral = require('../model/Electrodomestico')
 
 
-function agregarElectrodomesticoGeneral(params) {
-    
+// Filtros
+const getTelevisores = (datos)=> datos.filter(data => data.tdt !== undefined);
+const getNeveras = (datos)=> datos.filter(data => data.capacidad !== undefined)
+const getElectrodomesticosGeneral = (datos) => 
+    datos.filter(data => 
+        (data.tdt === undefined && data.capacidad === undefined));
+
+
+
+
+module.exports = {
+    getElectrodomesticosGeneral, getNeveras , getTelevisores
 }
